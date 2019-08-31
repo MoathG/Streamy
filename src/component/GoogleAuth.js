e68110c033d48b86afb1e74e49fdc716f1724370
MoathG <gharibmoath@gmail.com>
2019-08-31 16:43:48 +0300

    handling auth status through redux

--- src/component/GoogleAuth.js
+++ src/component/GoogleAuth.js
@@ -1,9 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { signIn, signOut } from '../actions';
 
-export default class GoogleAuth extends Component {
-    state = {
-        isSignedIn: null
-    }
+class GoogleAuth extends Component {
 
     componentDidMount() {
         window.gapi.load('client:auth2', () => {
@@ -12,14 +11,18 @@ export default class GoogleAuth extends Component {
                 scope: 'email'
             }).then( () => {
                 this.auth = window.gapi.auth2.getAuthInstance();
-                this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+                this.onAuthChange(this.auth.isSignedIn.get());
                 this.auth.isSignedIn.listen(this.onAuthChange)
             })
         });
     }
 
-    onAuthChange = () => {
-        this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+    onAuthChange = (isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn();
+        } else {
+            this.props.signOut();
+        }
     }
 
     onSignInClick = () => {
@@ -31,9 +34,9 @@ export default class GoogleAuth extends Component {
     }
 
     renderAuthButton() {
-        if (this.state.isSignedIn === null) {
+        if (this.props.isSignedIn === null) {
             return <div> I Don't know if we are signed in </div>
-        } else if (this.state.isSignedIn) {
+        } else if (this.props.isSignedIn) {
             return <button onClick={this.onSignOutClick} className='ui red google button'>
                 <i className='google icon' />
                 Sign Out
@@ -54,3 +57,12 @@ export default class GoogleAuth extends Component {
         )
     }
 }
+
+const mapStateToProps = (state) => {
+    return { isSignedIn: state.auth.isSignedIn };
+}
+
+export default connect(
+    mapStateToProps,
+    { signIn, signOut }
+) (GoogleAuth)
